0e984a0300bd3ede65e9af726f5275165335a282
@rootAccess <EricMouledoux@Gmail.com>
2019-04-16 13:32:00 -0500

    ported lazer input to go controller

---
+++ Assets/Scripts/ViveHandInteractionLaserPointer.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(OVRTrackedRemote), typeof(LineRenderer))]
+public class ViveHandInteractionLaserPointer : MonoBehaviour
+{
+    private GameObject m_targetObject;
+    private bool m_isHoldingSomething = false;
+
+    private RaycastHit m_raycast;
+
+    private OVRTrackedRemote m_remote;
+    private LineRenderer m_lineRenderer;
+    private Vector3 m_endLinePos;
+
+    private float m_timeOut;
+
+    static bool hasTriggered = false;
+
+    private bool m_controllerConnected
+    {
+        get { return OVRInput.IsControllerConnected(m_remote.m_controller); }
+    }
+
+    // ---------- ---------- ---------- ---------- ----------
+    void Start ()
+    {
+        m_remote = GetComponent<OVRTrackedRemote>();
+        m_lineRenderer = GetComponent<LineRenderer>();
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    void Update()
+    {
+        #region original lazer
+        //if (m_controllerConnected)
+        //{
+        //    if (CheckObjectHit())
+        //    {
+        //        if (CheckForInteractableObject(m_targetObject))
+        //        {
+        //            if (CheckInput())
+        //            {
+        //                OnObjectInteract();
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        //UpdateLaser();
+        #endregion
+
+
+        if (m_controllerConnected)
+        {
+            if (CheckLongInput())   // We are holding down the trigger
+            {
+                m_timeOut+= Time.deltaTime;
+                CheckObjectHit();
+            }
+
+            else if (CheckOffInput() && m_targetObject != null) // We have let go of the trigger, AND have a target object
+            {
+                if (CheckForInteractableObject(m_targetObject) != null && m_timeOut < 8f)
+                {
+                    OnObjectInteract();
+                }
+            }
+
+            else
+            {
+                m_timeOut = 0f;
+            }
+        }
+
+        bool[] extraLazerConditions = { CheckLongInput() };
+        UpdateLaser(extraLazerConditions);
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public bool CheckObjectHit()
+    {
+        if (Physics.Raycast(m_remote.transform.position, m_remote.transform.forward, out m_raycast))
+        {
+            if (m_targetObject != m_raycast.transform.gameObject && !m_isHoldingSomething && m_timeOut < 4f)
+            {
+                if (m_targetObject != null)
+                {
+                    Mouledoux.Components.Mediator.instance.NotifySubscribers
+                        (m_targetObject.GetInstanceID().ToString() + "->offhighlight", new Mouledoux.Callback.Packet());
+                }
+                
+                m_targetObject = m_raycast.transform.gameObject;
+
+                Mouledoux.Components.Mediator.instance.NotifySubscribers
+                    (m_targetObject.GetInstanceID().ToString() + "->onhighlight", new Mouledoux.Callback.Packet());
+            }
+
+            m_endLinePos = m_raycast.point;
+            return true;
+        }
+
+        else if (m_targetObject != null)
+        {
+            Mouledoux.Components.Mediator.instance.NotifySubscribers
+                (m_targetObject.GetInstanceID().ToString() + "->offhighlight", new Mouledoux.Callback.Packet());
+
+            m_targetObject = null;
+        }
+
+        m_endLinePos = m_remote.transform.position + m_remote.transform.forward;
+        return false;
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public InteractableObject CheckForInteractableObject(GameObject go)
+    {
+        return go.GetComponent<InteractableObject>();
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public bool CheckInput()
+    {
+        //m_hand.controller.TriggerHapticPulse();
+        //return (m_hand.grabPinchAction.stateDown);
+        return OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public bool CheckLongInput()
+    {
+        //m_hand.controller.TriggerHapticPulse();
+        //eturn (m_hand.GetBestGrabbingType() == Valve.VR.InteractionSystem.GrabTypes.Pinch);
+        return OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public bool CheckOffInput()
+    {
+        //m_hand.controller.TriggerHapticPulse();
+        //return (m_hand.grabPinchAction.stateUp);
+        return OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public int OnObjectInteract()
+    {
+        InteractableObject io = m_targetObject.GetComponent<InteractableObject>();
+
+        Mouledoux.Components.Mediator.instance.NotifySubscribers
+            (m_targetObject.GetInstanceID().ToString() + "->oninteract", new Mouledoux.Callback.Packet());
+
+        if (io.m_interactionType == InteractableObject.InteractionType.PICKUP && !io.m_lockedInPlace)
+        {
+            if (m_isHoldingSomething) return -1;
+            io.m_lockedInPlace = true;
+
+            StartCoroutine(HoldObject(m_raycast.collider));
+        }
+        
+        else if (io.m_interactionType == InteractableObject.InteractionType.LONGINTERACT)
+        {
+            StartCoroutine(LongInteract(m_raycast.transform.gameObject));
+        }
+
+        else
+        {
+            StartCoroutine(OffInteract(m_raycast.transform.gameObject));
+        }
+
+        return 0;
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public void UpdateLaser(bool[] extraConditions)
+    {
+        bool extraCondition = true;
+        foreach(bool condition in extraConditions)
+        {
+            if (condition == false)
+            {
+                extraCondition = false;
+                break;
+            }
+        }
+
+        m_lineRenderer.enabled = m_controllerConnected && extraCondition;
+        m_lineRenderer.SetPositions( new Vector3[] {m_remote.transform.position, m_endLinePos});
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public System.Collections.IEnumerator HoldObject(Collider go)
+    {
+        Vector3 lastPos = Vector3.zero;
+        Collider collider = go;
+        Color lineColor = m_lineRenderer.endColor;
+
+        collider.enabled = false;
+        m_isHoldingSomething = true;
+
+        bool canDrop = false;
+        while (CheckInput() || canDrop ==  false)
+        {
+            go.transform.position = m_lineRenderer.GetPosition(m_lineRenderer.positionCount - 1);
+            m_lineRenderer.endColor = canDrop ? Color.green : Color.red;
+
+            yield return null;
+        }
+
+        Mouledoux.Components.Mediator.instance.NotifySubscribers
+            (go.gameObject.GetInstanceID().ToString() + "->offinteract", new Mouledoux.Callback.Packet());
+
+        go.transform.parent = m_raycast.transform;
+        go.transform.position = m_raycast.point;
+
+        collider.enabled = true;
+        m_isHoldingSomething = false;
+        m_lineRenderer.endColor = lineColor;
+
+
+        if (!hasTriggered)
+        {
+            Mouledoux.Components.Mediator.instance.NotifySubscribers("trigger", new Mouledoux.Callback.Packet());
+            hasTriggered = true;
+        }
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public System.Collections.IEnumerator LongInteract(GameObject go)
+    {
+        yield return new WaitWhile(() => (CheckLongInput()));
+
+        Mouledoux.Components.Mediator.instance.NotifySubscribers
+            (go.GetInstanceID().ToString() + "->offinteract", new Mouledoux.Callback.Packet());
+
+        Mouledoux.Components.Mediator.instance.NotifySubscribers
+            (m_raycast.transform.gameObject.GetInstanceID().ToString() + "->offinteract", new Mouledoux.Callback.Packet());
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public System.Collections.IEnumerator OffInteract(GameObject go)
+    {
+        yield return new WaitWhile(() => (CheckInput()));
+
+        Mouledoux.Components.Mediator.instance.NotifySubscribers
+            (go.GetInstanceID().ToString() + "->offinteract", new Mouledoux.Callback.Packet());
+    }
+
+
+    // ---------- ---------- ---------- ---------- ----------
+    public void OnDestroy()
+    {
+        hasTriggered = false;
+    }
+}
