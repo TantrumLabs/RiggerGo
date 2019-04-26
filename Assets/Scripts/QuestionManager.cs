72175f44ce652e723f63447397aa39dbc539c768
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-04-26 10:10:39 -0500

    Question fixes and Cycler

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    public List<GameObject> m_questionGameObjects =  new List<GameObject>();
+
+    public UnityEngine.Events.UnityEvent m_onEmpty;
+
+    void Start(){
+        if(m_questionGameObjects.Count <= 0){
+            m_onEmpty.Invoke();
+            return;
+        }
+
+        foreach(GameObject go in m_questionGameObjects)
+            go.SetActive(false);
+
+        m_questionGameObjects[0].SetActive(true);
+    }
+
+    public void RemoveCurrentQuestion(){
+        Debug.Log("Next Question");
+        m_questionGameObjects[0].SetActive(false);
+
+        m_questionGameObjects.Remove(m_questionGameObjects[0]);
+
+        if(m_questionGameObjects.Count <= 0){
+            m_onEmpty.Invoke();
+            return;
+        }
+
+        m_questionGameObjects[0].SetActive(true);
+    }
+}
