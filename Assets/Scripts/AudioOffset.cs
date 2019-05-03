5f90afcec7e66ae80b1b783979d9380b4971ce09
@rootAccess <EricMouledoux@Gmail.com>
2019-05-03 11:49:12 -0500

    audio offset

---
+++ Assets/Scripts/AudioOffset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOffset : MonoBehaviour
+{
+    public AudioSource m_audioSource;
+    private float m_playBackTime;
+
+
+    void Update()
+    {
+        if(m_audioSource.isPlaying)
+        {
+            if(m_audioSource.time >= m_playBackTime)
+            {
+                m_audioSource.Stop();
+            }
+        }
+    }
+
+
+    public void StartAudioAt(float sec)
+    {
+        m_audioSource.Stop();
+        m_audioSource.time = sec;
+        m_audioSource.Play();
+    }
+
+    public void SetAudioOffset(float timeOffset)
+    {
+        m_playBackTime = m_audioSource.time + timeOffset;
+    }
+
+}
