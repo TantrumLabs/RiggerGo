22c72c901698f28e08282632c653a8b24bd6c779
Anthony-Touchet <anthonyjtouchet@gmail.com>
2019-05-13 11:37:13 -0700

    Logging Data
    Ability to capture both answer given and question. Also able to save and load locally.

---
+++ Assets/Scripts/DataLogging/QuestionHazardData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionHazardData
+{
+    public string m_questionsMissed ="", m_hazardsMissed = "";
+    public int m_score = 0, m_maxScore = 0, m_questionCount = 0, m_hazardCount = 0;
+}
---
+++ Assets/Scripts/DataLogging/SaveLocally.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class SaveLocally : MonoBehaviour
+{
+    private static string path = Application.persistentDataPath + "/";
+
+    public static void SaveScoreData(QuestionHazardData data, string email){
+        string m_localDataPath;
+
+        DirectoryInfo dI = new DirectoryInfo(path);
+        FileInfo[] files = dI.GetFiles("*.xml");
+
+        int count = 0;
+        foreach(FileInfo file in files)
+            count++;
+
+        m_localDataPath = (count + 1).ToString("D4") + "_" + email+".xml";
+
+        //Serialize 
+        XmlSerializer xs = new XmlSerializer(typeof(QuestionHazardData));
+        TextWriter tw = new StreamWriter(path+m_localDataPath);
+        xs.Serialize(tw, data);
+        tw.Close();
+    }
+
+    public static QuestionHazardData LoadScoreData(string fileName){
+        var result = new QuestionHazardData();
+        XmlSerializer xs = new XmlSerializer(typeof(QuestionHazardData));
+
+        using(var sr = new StreamReader(path + fileName))
+        {
+            result = (QuestionHazardData)xs.Deserialize(sr);
+            sr.Close();
+        }
+
+        return result;
+    }
+
+    public static void DeleteFile(string fileName){
+        File.Delete(fileName);
+    }
+}
